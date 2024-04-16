2d4e5813abc7c2fa55c6749053eda90b0d69ed35
Elias <eliasbg2000@gmail.com>
2024-04-16 17:10:48 +0200

    botones

--- www/js/index.js
+++ www/js/index.js
@@ -8,5 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    const elementosA = document.querySelectorAll('a');
+    elementosA.forEach(elemento => {
+        elemento.addEventListener('mouseenter', toggleClase);
+        elemento.addEventListener('mouseleave', toggleClase);
+    });
 
+    function toggleClase(event) {
+        const elemento = event.target;
+
+        if (elemento.classList.contains('animate__slow')) {
+            elemento.classList.remove('animate__slow');
+            elemento.classList.add('animate__faster');
+        } else {
+            elemento.classList.remove('animate__faster');
+            elemento.classList.add('animate__slow');
+        }
+    }
 });
