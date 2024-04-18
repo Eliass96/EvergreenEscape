b7bab40c205df933426e5861a59f6fe6e6197b56
Usuario <Usuario@DESKTOP-KGTO1NR>
2024-04-18 09:57:34 +0200

    Hecho db y app a falta de login

---
+++ www/js/pug/globalRankings.js
@@ -0,0 +1,21 @@
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;function crearPuntuacionesGlobales(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;var locals_for_with = (locals || {});(function (puntuaciones) {pug_html = pug_html + "\u003Coutput class=\"cardRanking\" id=\"rankingGlobal\"\u003E\u003Cdiv class=\"card-inner\"\u003E\u003Cheader class=\"d-flex align-items-center justify-content-center\"\u003E\u003Ch2 class=\"titulo_card_ranking\"\u003EMejores jugadores del mundo\u003C\u002Fh2\u003E\u003C\u002Fheader\u003E\u003Col class=\"lista_ranking\"\u003E";
+// iterate puntuaciones
+;(function(){
+  var $$obj = puntuaciones;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index0 = 0, $$l = $$obj.length; pug_index0 < $$l; pug_index0++) {
+        var punt = $$obj[pug_index0];
+pug_html = pug_html + "\u003Cli class=\"texto_ranking d-flex align-items-center justify-content-between\"\u003E\u003Cp\u003EJugador 1\u003C\u002Fp\u003E\u003Cp\u003E" + (pug_escape(null == (pug_interp = punt + ' puntos') ? "" : pug_interp)) + "\u003C\u002Fp\u003E\u003C\u002Fli\u003E";
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index0 in $$obj) {
+      $$l++;
+      var punt = $$obj[pug_index0];
+pug_html = pug_html + "\u003Cli class=\"texto_ranking d-flex align-items-center justify-content-between\"\u003E\u003Cp\u003EJugador 1\u003C\u002Fp\u003E\u003Cp\u003E" + (pug_escape(null == (pug_interp = punt + ' puntos') ? "" : pug_interp)) + "\u003C\u002Fp\u003E\u003C\u002Fli\u003E";
+    }
+  }
+}).call(this);
+
+pug_html = pug_html + "\u003C\u002Fol\u003E\u003C\u002Fdiv\u003E\u003C\u002Foutput\u003E";}.call(this,"puntuaciones" in locals_for_with?locals_for_with.puntuaciones:typeof puntuaciones!=="undefined"?puntuaciones:undefined));;return pug_html;}
--- www/js/rankings.js
+++ www/js/rankings.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", async function () {
     let outputRanking = document.getElementById("rankingPersonal");
+    let outputRankingGlobal = document.getElementById("rankingGlobal");
 
     try {
         let idUsuario = localStorage.getItem('isLogged');
@@ -11,6 +12,25 @@ document.addEventListener("DOMContentLoaded", async function () {
         console.log(datosPuntuacion);
         const html = crearPuntuaciones({puntuaciones: datosPuntuacion});
         outputRanking.innerHTML = html;
+    } catch (error) {
+        console.log(error);
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Algo ha fallado, pruebe a reiniciar la página"
+        });
+    }
+
+
+    try {
+        const resp = await fetch(`/puntuaciones`);
+        if (!resp.ok) {
+            throw new Error("Error al cargar");
+        }
+        const datosPuntuacion = await resp.json();
+        console.log(datosPuntuacion);
+        const html1 = crearPuntuacionesGlobales({puntuaciones: datosPuntuacion})
+        outputRankingGlobal.innerHTML = html1;
     } catch (error) {
         console.log(error);
         Swal.fire({
