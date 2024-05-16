4cca571ca35dfa3e95219c84a20d2808bfab4794
Elias <eliasbg2000@gmail.com>
2024-05-16 09:59:13 +0200

    banderitas en español

--- www/js/appPaises.js
+++ www/js/appPaises.js
@@ -1,58 +1,84 @@
 const paises = document.getElementById('listadoPaises');
 let formulario;
 let data;
+let dataTraducido;
 document.addEventListener('DOMContentLoaded', async () => {
     await fetchData();
     formulario = document.getElementById('formularioPaises');
     formulario.addEventListener('input', () => {
-        buscarPaises(data);
+        buscarPaises(data, dataTraducido);
     })
 })
 
 const fetchData = async () => {
     try {
-        const response = await fetch('../api.json');
+        const response = await fetch('../apiPaises.json');
+        const responseTrad = await fetch('../apiPaisesTraducida.json');
         data = await response.json();
-        listaPaises(data);
+        dataTraducido = await responseTrad.json();
+        listaPaises(data, dataTraducido);
     } catch (error) {
         console.log(error);
     }
 }
 
-const listaPaises = data => {
-    console.log(data)
-    data.sort((a, b) => {
-        if (a.name.common < b.name.common) return -1;
-        if (a.name.common > b.name.common) return 1;
+const listaPaises = (data, dataTraducido) => {
+    dataTraducido.sort((a, b) => {
+        if (a.translations.es < b.translations.es) return -1;
+        if (a.translations.es > b.translations.es) return 1;
         return 0;
     });
     let elementos = ``
-    data.forEach(item => {
-        if (item.name.common !== null && item.name.common !== undefined) {
-            elementos += `
-            <a class="opcionentradatexto d-flex align-items-center justify-content-start dropdown-item ms-1 me-1" id="pais">
-                <img src="${item.flags.svg}" alt="${item.flags.alt}" class="bandera me-4">
-                <p class="flex-wrap">${item.name.common}</p>
-            </a>
-        `;
+    dataTraducido.forEach(itemEsp => {
+        if (itemEsp.translations.es !== null && itemEsp.translations.es !== undefined) {
+            data.forEach(item => {
+                if (item.name.common !== null && item.name.common !== undefined) {
+                    if (item.name.common === itemEsp.name) {
+                        elementos += `
+                            <a class="opcionentradatexto d-flex align-items-center justify-content-start dropdown-item ms-1 me-1" id="pais">
+                                <img src="${item.flags.svg}" alt="${item.flags.alt}" class="bandera me-4">
+                                <p class="flex-wrap">${itemEsp.translations.es}</p>
+                            </a>
+                        `;
+                    }
+                }
+            });
         }
     });
     paises.innerHTML = elementos
 }
 
-const buscarPaises = data => {
+const buscarPaises = (data, dataTraducido) => {
     formulario.addEventListener('keyup', e => {
         e.preventDefault()
         const letra = formulario.value.toLowerCase()
-        const arrayFiltrado = data.filter(item => {
-            const letraApi = item.name.common ? item.name.common.toLowerCase() : '';
+        const arrayFiltrado = dataTraducido.filter(item => {
+            const letraApi = item.translations.es ? item.translations.es.toLowerCase() : '';
             return letraApi.includes(letra);
         });
         const listaOrdenada = arrayFiltrado.sort((a, b) => {
-            if (a.name.common < b.name.common) return -1;
-            if (a.name.common > b.name.common) return 1;
+            if (a.translations.es < b.translations.es) return -1;
+            if (a.translations.es > b.translations.es) return 1;
             return 0;
         });
-        listaPaises(listaOrdenada);
+        //listaPaises(listaOrdenada);
+        let elementos = ``
+        listaOrdenada.forEach(itemEsp => {
+            if (itemEsp.translations.es !== null && itemEsp.translations.es !== undefined) {
+                data.forEach(item => {
+                    if (item.name.common !== null && item.name.common !== undefined) {
+                        if (item.name.common === itemEsp.name) {
+                            elementos += `
+                                <a class="opcionentradatexto d-flex align-items-center justify-content-start dropdown-item ms-1 me-1" id="pais">
+                                    <img src="${item.flags.svg}" alt="${item.flags.alt}" class="bandera me-4">
+                                    <p class="flex-wrap">${itemEsp.translations.es}</p>
+                                </a>
+                            `;
+                        }
+                    }
+                });
+            }
+        });
+        paises.innerHTML = elementos
     })
 }
