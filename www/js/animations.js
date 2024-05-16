60da087bd56baa4fb782c7e923bc2b0454d883fb
Elias <eliasbg2000@gmail.com>
2024-05-16 10:04:37 +0200

    fichero animations hecho

---
+++ www/js/animations.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const elementosA = document.querySelectorAll('a');
+    elementosA.forEach(elemento => {
+        elemento.addEventListener('mouseenter', toggleClase);
+        elemento.addEventListener('mouseleave', toggleClase);
+    });
+
+    function toggleClase(event) {
+        const elemento = event.target;
+        if (elemento.classList.contains('animate__slow')) {
+            elemento.classList.remove('animate__slow');
+            elemento.classList.add('animate__faster');
+        } else if (elemento.classList.contains('animate__faster')) {
+            elemento.classList.remove('animate__faster');
+            elemento.classList.add('animate__slow');
+        }
+    }
+});
--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const elementosA = document.querySelectorAll('a');
-
     if (document.getElementById('butUser')) {
         try {
 
@@ -45,22 +44,6 @@ document.addEventListener('DOMContentLoaded', async function () {
                 title: "Oops...",
                 text: "Algo ha fallado, pruebe a reiniciar la página"
             });
-        }
-    }
-
-    elementosA.forEach(elemento => {
-        elemento.addEventListener('mouseenter', toggleClase);
-        elemento.addEventListener('mouseleave', toggleClase);
-    });
-
-    function toggleClase(event) {
-        const elemento = event.target;
-        if (elemento.classList.contains('animate__slow')) {
-            elemento.classList.remove('animate__slow');
-            elemento.classList.add('animate__faster');
-        } else if (elemento.classList.contains('animate__faster')) {
-            elemento.classList.remove('animate__faster');
-            elemento.classList.add('animate__slow');
         }
     }
 });
